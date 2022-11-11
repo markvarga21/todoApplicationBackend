c90a20efc034d6c0150a943dc9597663aa756aa5
Varga József Márk <vmark2145@gmail.com>
2022-11-11 12:01:23 +0100

    Spring boot skeleton app

---
+++ src/test/java/com/markvarga21/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markvarga21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
