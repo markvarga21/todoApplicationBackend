542dc43bd5a3d39904d39854873b53bed715f4fc
Varga József Márk <vmark2145@gmail.com>
2022-11-12 15:00:50 +0200

    Added spring security dependencies to pom.xml

---
+++ src/main/java/com/markvarga21/util/converter/LocalDateTimeConverter.java
@@ -0,0 +1,21 @@
+package com.markvarga21.util.converter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+@RequiredArgsConstructor
+public class LocalDateTimeConverter {
+    private final DateTimeFormatter formatter;
+
+    public String convertDateToString(LocalDateTime dateToFormat) {
+        return dateToFormat.format(this.formatter);
+    }
+
+    public LocalDateTime convertDateStringToLocalDateTime(String dateToConvert) {
+        return LocalDateTime.parse(dateToConvert, this.formatter);
+    }
+}
